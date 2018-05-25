c100375cbc24e00bc186f8fa410bb6e56082c1ae
Elad Matia <elad.matia@gmail.com>
2018-05-25 11:28:32 +0300

    Hello

--- Projekt/Project/prog.c
+++ Projekt/Project/prog.c
@@ -20,6 +20,7 @@ char* readBinaryData(FILE* signature, char* signatureArr, int length);
 int isFileInfected(char* virusSignature, char* file, int sigLength, int fileLength);
 int getFileLength(FILE* file);
 FILE* createLog(char* folderPath, FILE* log, char* virusPath, int choice);
+char* getPercent(char* file, int length, int mode, char* tempArray, int* globalLength);
 
 int main(int argc, char** argv)
 {
@@ -34,10 +35,15 @@ int main(int argc, char** argv)
 	int counter = 0;
 	int choice = 0;
 	char* logPath = 0;
+	char* tempArray = 0;
+	int state = 0;
+	int tempArrayLength = 0;
+	int ok = FALSE;
 	//check if we have enough arguments:
 	if (argc != 3)
 	{
 		printf("Invalid execution!\nUsage: antivirus.exe <directory_to_search> <VirusSignature>");
+		getchar();
 		return 1;
 	}
 	//check if the folder or file can be opened:
@@ -67,64 +73,99 @@ int main(int argc, char** argv)
 	sigLength = getFileLength(signature); // file length
 	virusSignature = readBinaryData(signature, virusSignature, sigLength); // the virus signature is now inside array
 
-	if (!choice)
+	while ((pDirent = readdir(pDir)) != NULL) //read from the directory until we don't get a null
 	{
-		while ((pDirent = readdir(pDir)) != NULL) //read from the directory until we don't get a null
+		if (counter >= 2)
 		{
-			if (counter >= 2)
+			skip = FALSE;
+			exPath = (char*)malloc(strlen(folderPath) + strlen(pDirent->d_name) + 2); // +2 for the 0 and for the //
+			strcpy(exPath, folderPath);
+			strcat(exPath, "/");
+			strcat(exPath, pDirent->d_name);
+			//exPath - now has the path to the first file 
+			//read the contend of the file:
+			FILE* fileToCheck = fopen(exPath, "rb");
+			if (fileToCheck == NULL)
 			{
-				skip = FALSE;
-				exPath = (char*)malloc(strlen(folderPath) + strlen(pDirent->d_name) + 2); // +2 for the 0 and for the //
-				strcpy(exPath, folderPath);
-				strcat(exPath, "\\");
-				strcat(exPath, pDirent->d_name);
-				//exPath - now has the path to the first file 
-
-
-
-				//>>>>>FROM HERE SECTION B HAS TO CHANGE SOME STUFF
-				//read the contend of the file:
-				FILE* fileToCheck = fopen(exPath, "rb");
-				if (fileToCheck == NULL)
+				printf("Couldn't open %s. Skipping (Could be a directory or something)\n", exPath);
+				skip = TRUE;
+			}
+			if (!skip) //if the file could be openned
+			{
+				fileLength = getFileLength(fileToCheck);
+				fileCheck = readBinaryData(fileToCheck, fileCheck, fileLength); // read its contents
+				if (!choice) //if the user chose normal scan
 				{
-					printf("Couldn't open %s. Skipping (Could be a directory or something)\n", exPath);
-					skip = TRUE;
-				}
-				if (!skip)
-				{
-					fileLength = getFileLength(fileToCheck);
-					fileCheck = readBinaryData(fileToCheck, fileCheck, fileLength);
-					if (isFileInfected(virusSignature, fileCheck, sigLength, fileLength))
+					if (isFileInfected(virusSignature, fileCheck, sigLength, fileLength)) // check if the file is infected
 					{
 						printf("%s - Infected!\n", exPath);
-						fprintf(log, "%s   Infected!\n", exPath);
+						fprintf(log, "%s   Infected!\n", exPath); //write it to the log
 					}
-					else
+					else //if the file is clear
 					{
 						printf("%s - Clean\n", exPath);
 						fprintf(log, "%s   Clean\n", exPath);
 					}
-					fclose(fileToCheck);
+
 				}
-				
+				else // if the user chose quick scan: 
+				{
+					tempArray = getPercent(fileCheck, fileLength, state, tempArray, &tempArrayLength); // recieve the first 20 percent of the array
+					if (isFileInfected(virusSignature, tempArray, sigLength, tempArrayLength)) // if the virus was found in the first 20 percent, infrom the user and write it to the log
+					{
+						printf("%s - Infected! <first 20%%>\n", exPath);
+						fprintf(log, "%s   Infected! <first 20%%>\n", exPath);
+						ok = TRUE;
+
+					}
+					if (!ok) // if the virus wasn't found, search in the last 20 percnet
+					{
+						state = 1;
+						tempArray = getPercent(fileCheck, fileLength, state, tempArray, &tempArrayLength); // recieve the last 20 percent of the array
+						if (isFileInfected(virusSignature, tempArray, sigLength, tempArrayLength))
+						{
+							printf("%s - Infected! <last 20%%>\n", exPath);
+							fprintf(log, "%s   Infected! <last 20%%>\n", exPath);
+							ok = TRUE;
+						}
+
+					}
+					if (!ok) // if the virus still wasn't found, run a full scan.
+					{
+						//run a full scan
+						if (isFileInfected(virusSignature, fileCheck, sigLength, fileLength))
+						{
+							printf("%s - Infected!\n", exPath);
+							fprintf(log, "%s   Infected!\n", exPath);
+						}
+						else
+						{
+							printf("%s - Clean\n", exPath);
+							fprintf(log, "%s   Clean\n", exPath);
+						}
+
+
+					}
+					ok = FALSE;
+				}
+				fclose(fileToCheck); // close the file that was scanned
 			}
 
-			counter++; //the first two "files" are just .. so we are skipping
 		}
-	}
-	else
-	{
-		printf("Coming soon.");
-	}
-	printf("Scan complete");
 
+		counter++; //the first two "files" are just .. so we are skipping
+	}
+
+	printf("Scan complete\n");
+	printf("See log path for results: %s\\AntiVirusLog.txt", folderPath);
 
 
 	getchar();
-	fclose(log);
-	free(exPath);
-	free(virusSignature);
-	free(fileCheck);
+	fclose(log); // close the log file
+	free(tempArray); // close the tempArray (where the last or first 20 percent of the file data was stored)
+	free(exPath); // free the path for the files
+	free(virusSignature); // free the virus signature data array
+	free(fileCheck); // 
 	return 0;
 }
 /*
@@ -140,7 +181,7 @@ char* readBinaryData(FILE* sourceFile, char* destinationArray, int length)
 
 //https://stackoverflow.com/questions/7483634/determining-if-one-array-is-contained-in-another
 /*
-The function checks if specific file is infected 
+The function checks if specific file is infected
 Input: the virus signatue, the file to check and both of their lengths
 Output: 0 if file is clean, anything otherwise
 */
@@ -190,10 +231,10 @@ FILE* createLog(char* folderPath, FILE* log, char* virusPath, int choice)
 	char* exPath;
 	exPath = (char*)malloc(strlen(folderPath) + 2 + strlen("AntiVirusLog.txt")); // +2 for the 0 and for the //
 	strcpy(exPath, folderPath);
-	strcat(exPath, "\\");
+	strcat(exPath, "/");
 	strcat(exPath, "AntiVirusLog.txt");
 
-	char mode[LENGTH] = {0};
+	char mode[LENGTH] = { 0 };
 	if (!choice)
 	{
 		strcat(mode, "Normal Scan");
@@ -205,10 +246,40 @@ FILE* createLog(char* folderPath, FILE* log, char* virusPath, int choice)
 
 
 	log = fopen(exPath, "w");
-	fprintf(log, "Anti-Virus began! Welcome!\nFolder to scan:\n%s\nVirus Signature:\n%s\nScanning option:\n%s\nResults:\n",folderPath, virusPath, mode);
+	fprintf(log, "Anti-Virus began! Welcome!\nFolder to scan:\n%s\nVirus Signature:\n%s\nScanning option:\n%s\nResults:\n", folderPath, virusPath, mode);
 
 
 
 	free(exPath);
 	return log;
+}
+/*
+This function returns the first\last percent of the file array data according to mode
+Input: the file array data, its length, mode- 0 for first 20 percent other for the last 20 percent. the array to store stuff and its length
+*/
+char* getPercent(char* file, int length, int mode, char* tempArray, int* globalLength)
+{
+	int i = 0;
+	int newLength = 0;
+	int counter = 0;
+	newLength = (int)(0.2 * length);
+	tempArray = (char*)malloc(sizeof(char)*newLength);
+	// if mode is 0 - we need to return the first 20 percent of the array
+	if (!mode)
+	{
+		for (i = 0; i < newLength; i++)
+		{
+			tempArray[i] = file[i];
+		}
+	}
+	else
+	{
+		for (i = length - newLength; i < length; i++)
+		{
+			tempArray[counter] = file[i];
+			counter++;
+		}
+	}
+	*globalLength = newLength;
+	return tempArray;
 }
