b4b79d940e780fbdf7c0dfa9e3ac7d57abf3bc9a
Elad Matia <elad.matia@gmail.com>
2018-05-25 21:14:16 +0300

    Works?

--- Projekt/Project/prog.c
+++ Projekt/Project/prog.c
@@ -15,9 +15,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <dirent.h>
-
 char* readBinaryData(FILE* signature, char* signatureArr, int length);
-char* sort(DIR* dir, dirent* pDirent, char* filesArray);
+char** sort(DIR* dir, char** filesArray, int* length);
 int isFileInfected(char* virusSignature, char* file, int sigLength, int fileLength);
 int getFileLength(FILE* file);
 FILE* createLog(char* folderPath, FILE* log, char* virusPath, int choice);
@@ -26,22 +25,17 @@ char* getPercent(char* file, int length, int mode, char* tempArray, int* globalL
 
 int main(int argc, char** argv)
 {
-<<<<<<< Updated upstream
-	struct dirent *pDirent;
 	FILE* log = 0;
-=======
-	char path[] = "C:\\Users\\magshimim\\OneDrive\\Documents\\Magshimim\\C2\\C\\Project\\Files\\Project_Files\\KittenVirusSign";
-	char picPath[] = "C:\\Users\\magshimim\\OneDrive\\Documents\\Magshimim\\C2\\C\\Project\\Files\\Project_Files\\kitten_frog.jpg";
->>>>>>> Stashed changes
 	char* virusSignature = 0;
 	char* fileCheck = 0;
 	int sigLength = 0;
 	int fileLength = 0;
+	int length = 0;
 	int skip = FALSE;
 	char* exPath = 0;
 	int choice = 0;
-	char* logPath = 0;
-	char* filesArray = 0;
+	int i = 0;
+	char** filesArray = 0;
 	char* tempArray = 0;
 	int state = 0;
 	int tempArrayLength = 0;
@@ -79,88 +73,86 @@ int main(int argc, char** argv)
 
 	sigLength = getFileLength(signature); // file length
 	virusSignature = readBinaryData(signature, virusSignature, sigLength); // the virus signature is now inside array
-	// dont't show the dots
-	readdir(pDir);
-	readdir(pDir);
-	filesArray = sort(pDir, pDirent, filesArray);
-	while ((pDirent = readdir(pDir)) != NULL) //read from the directory until we don't get a null
+
+	filesArray = sort(pDir, filesArray, &length);
+	for (i = 0; i < length; i++) //read from the directory until we don't get a null
 	{
-			skip = FALSE;
-			exPath = (char*)malloc(strlen(folderPath) + strlen(pDirent->d_name) + 2); // +2 for the 0 and for the //
-			strcpy(exPath, folderPath);
-			strcat(exPath, "/");
-			strcat(exPath, pDirent->d_name);
-			//exPath - now has the path to the first file 
-			//read the contend of the file:
-			FILE* fileToCheck = fopen(exPath, "rb");
-			if (fileToCheck == NULL)
+		skip = FALSE;
+		exPath = (char*)malloc(strlen(folderPath) + strlen(filesArray[i]) + 2); // +2 for the 0 and for the //
+		strcpy(exPath, folderPath);
+		strcat(exPath, "/");
+		strcat(exPath, filesArray[i]);
+		//exPath - now has the path to the first file 
+		//read the contend of the file:
+		FILE* fileToCheck = fopen(exPath, "rb");
+		if (fileToCheck == NULL)
+		{
+			printf("Couldn't open %s. Skipping (Could be a directory or something)\n", exPath);
+			skip = TRUE;
+		}
+		if (!skip && (strcmp(filesArray[i], "AntiVirusLog.txt"))) //if the file could be openned
+		{
+			fileLength = getFileLength(fileToCheck);
+			fileCheck = readBinaryData(fileToCheck, fileCheck, fileLength); // read its contents
+			if (!choice) //if the user chose normal scan
 			{
-				printf("Couldn't open %s. Skipping (Could be a directory or something)\n", exPath);
-				skip = TRUE;
-			}
-			if (!skip && (strcmp(pDirent->d_name, "AntiVirusLog.txt"))) //if the file could be openned
-			{
-				fileLength = getFileLength(fileToCheck);
-				fileCheck = readBinaryData(fileToCheck, fileCheck, fileLength); // read its contents
-				if (!choice) //if the user chose normal scan
+				if (isFileInfected(virusSignature, fileCheck, sigLength, fileLength)) // check if the file is infected
 				{
-					if (isFileInfected(virusSignature, fileCheck, sigLength, fileLength)) // check if the file is infected
+					printf("%s - Infected!\n", exPath);
+					fprintf(log, "%s   Infected!\n", exPath); //write it to the log
+				}
+				else //if the file is clear
+				{
+					printf("%s - Clean\n", exPath);
+					fprintf(log, "%s   Clean\n", exPath);
+				}
+
+			}
+			else // if the user chose quick scan: 
+			{
+				state = 0;
+				tempArray = getPercent(fileCheck, fileLength, state, tempArray, &tempArrayLength); // recieve the first 20 percent of the array
+				if (isFileInfected(virusSignature, tempArray, sigLength, tempArrayLength)) // if the virus was found in the first 20 percent, infrom the user and write it to the log
+				{
+					printf("%s - Infected! <first 20%%>\n", exPath);
+					fprintf(log, "%s   Infected! <first 20%%>\n", exPath);
+					ok = TRUE;
+
+				}
+				if (!ok) // if the virus wasn't found, search in the last 20 percnet
+				{
+					state = 1;
+					tempArray = getPercent(fileCheck, fileLength, state, tempArray, &tempArrayLength); // recieve the last 20 percent of the array
+					if (isFileInfected(virusSignature, tempArray, sigLength, tempArrayLength))
+					{
+						printf("%s - Infected! <last 20%%>\n", exPath);
+						fprintf(log, "%s   Infected! <last 20%%>\n", exPath);
+						ok = TRUE;
+					}
+
+				}
+				if (!ok) // if the virus still wasn't found, run a full scan.
+				{
+					//run a full scan
+					if (isFileInfected(virusSignature, fileCheck, sigLength, fileLength))
 					{
 						printf("%s - Infected!\n", exPath);
-						fprintf(log, "%s   Infected!\n", exPath); //write it to the log
+						fprintf(log, "%s   Infected!\n", exPath);
 					}
-					else //if the file is clear
+					else
 					{
 						printf("%s - Clean\n", exPath);
 						fprintf(log, "%s   Clean\n", exPath);
 					}
 
+
 				}
-				else // if the user chose quick scan: 
-				{
-					state = 0;
-					tempArray = getPercent(fileCheck, fileLength, state, tempArray, &tempArrayLength); // recieve the first 20 percent of the array
-					if (isFileInfected(virusSignature, tempArray, sigLength, tempArrayLength)) // if the virus was found in the first 20 percent, infrom the user and write it to the log
-					{
-						printf("%s - Infected! <first 20%%>\n", exPath);
-						fprintf(log, "%s   Infected! <first 20%%>\n", exPath);
-						ok = TRUE;
-
-					}
-					if (!ok) // if the virus wasn't found, search in the last 20 percnet
-					{
-						state = 1;
-						tempArray = getPercent(fileCheck, fileLength, state, tempArray, &tempArrayLength); // recieve the last 20 percent of the array
-						if (isFileInfected(virusSignature, tempArray, sigLength, tempArrayLength))
-						{
-							printf("%s - Infected! <last 20%%>\n", exPath);
-							fprintf(log, "%s   Infected! <last 20%%>\n", exPath);
-							ok = TRUE;
-						}
-
-					}
-					if (!ok) // if the virus still wasn't found, run a full scan.
-					{
-						//run a full scan
-						if (isFileInfected(virusSignature, fileCheck, sigLength, fileLength))
-						{
-							printf("%s - Infected!\n", exPath);
-							fprintf(log, "%s   Infected!\n", exPath);
-						}
-						else
-						{
-							printf("%s - Clean\n", exPath);
-							fprintf(log, "%s   Clean\n", exPath);
-						}
-
-
-					}
-					ok = FALSE;
-				}
-				fclose(fileToCheck); // close the file that was scanned
+				ok = FALSE;
 			}
-
+			fclose(fileToCheck); // close the file that was scanned
 		}
+
+	}
 
 	printf("Scan complete\n");
 	printf("See log path for results: %s\\AntiVirusLog.txt", folderPath);
@@ -291,15 +283,35 @@ char* getPercent(char* file, int length, int mode, char* tempArray, int* globalL
 	*globalLength = newLength;
 	return tempArray;
 }
-char* sort(DIR* dir, dirent* pDirent, char* filesArray)
+char** sort(DIR* dir, char** filesArray, int* length)
 {
 	int i = 0;
+	struct dirent *pDirent;
+	int numOfFiles = 0;
+
+	// dont't show the dots
+
 	while ((pDirent = readdir(dir)) != NULL)
 	{
-		filesArray = (char*)realloc(filesArray, len(pDirent->d_name));
-		filesArray[i] = pDirent->d_name;
-		printf("%s\n", filesArray[i]);
-		i++;
+		numOfFiles++;
+
 	}
+	filesArray = (char**)malloc(sizeof(char*)*numOfFiles);
+	rewinddir(dir);
+	//readdir(dir);
+	//readdir(dir);
+	pDirent = readdir(dir);
+	for(i=0; i<numOfFiles; i++)
+	{
+		filesArray[i] = (char*)malloc(strlen(pDirent->d_name)*sizeof(char)+1);
+		filesArray[i][strlen(pDirent->d_name) * sizeof(char)] = 0;
+		strncpy(filesArray[i], pDirent->d_name, strlen(pDirent->d_name));
+		if (i != numOfFiles)
+		{
+			pDirent = readdir(dir);
+
+		}
+	}
+	*length = i;
 	return filesArray;
 }
